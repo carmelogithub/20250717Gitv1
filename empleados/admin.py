c418c5bf627cdddb4885d2d899aeb789ec6bc3ca
Carmelo Es <carmelo.escribano@gmail.com>
2025-07-17 18:00:43 +0200

    templates basadas en base

--- empleados/admin.py
+++ empleados/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from empleados.models import Empleado, Departamento
+
+
+@admin.register(Empleado)
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'email', 'departamento')
+    list_filter = ('departamento',)
+
+admin.site.register(Departamento)
