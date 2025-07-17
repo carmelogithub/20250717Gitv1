6e76bacbfdbf6d69159a0aeeb03426063b492cd1
Carmelo Es <carmelo.escribano@gmail.com>
2025-07-17 18:08:45 +0200

    urls y views funcionando ok

---
+++ empleados/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+path('empleados/', views.empleados_list, name='empleados'),
+]
--- empleados/views.py
+++ empleados/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Empleado
+
+def empleados_list(request):
+    empleados = Empleado.objects.select_related('departamento')
+    return render(request, 'empleados/index.html', {'empleados': empleados})
